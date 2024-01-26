5d1c3fb4c4c5008c658d6dbd5759170178ae07b3
KelvinoKing <kelvinoking@outlook.com>
2024-01-27 00:54:06 +0300

    Deleted

--- models/__init__.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""uses file storage to manage data for my BookSwap project"""
-from models.engine import file_storage
-
-
-storage = file_storage.FileStorage()
-storage.reload()
